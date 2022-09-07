49fc05655f43b681f0a8e03a8099303b25a8b278
URK21CS7001-HAREESH KUMAR <110516162+hareesh123-ctrl@users.noreply.github.com>
2022-09-07 10:45:50 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -8,110 +8,45 @@ class Solution:
           rear: An integer which denotes the index of the element at the rear of the queue.
           size: An integer which represents the size of stack and queue.
       """
+stack = []
+queue = []
+top = -1
+rear = -1
+front = -1
+text = input("Enter the String: ")
+size = len(text)
 
-    # Write your code here
-    def __init__(self, size):
-        """Inits Solution with stack, queue, size, top, front and rear.
-        Arguments:
-          size: An integer to set the size of stack and queue.
-        """
-        self.stack = []
-        self.queue = []
-        self.size = size
-        self.top = -1
-        self.rear = -1
-        self.front = -1
-
-    def is_stack_empty(self):
-        """
-        Check whether the stack is empty.
-        Returns:
-          True if it is empty, else returns False.
-        """
-        # Write your code here
-
-    def is_queue_empty(self):
-        """
-        Check whether the queue is empty.
-        Returns:
-          True if it is empty, else returns False.
-        """
-        # Write your code here
-
-    def is_stack_full(self):
-        """
-        Check whether the stack is full.
-        Returns:
-          True if it is full, else returns False.
-        """
-        # Write your code here
-
-    def is_queue_full(self):
-        """
-        Check whether the queue is full.
-        Returns:
-          True if it is full, else returns False.
-        """
-        # Write your code here
-
-    def push_character(self, character):
-        """
-        Push the character to stack, if stack is not full.
-        Arguments:
-            character: A character that will be pushed to the stack.
-        """
-        # Write your code here
-
-    def enqueue_character(self, character):
-        """
-        Enqueue the character to queue, if queue is not full.
-        Arguments:
-            character: A character that will be enqueued to queue.
-        """
-        # Write your code here
-
-    def pop_character(self):
-        """
-        Do pop operation if the stack is not empty.
-        Returns:
-          The data that is popped out if the stack is not empty.
-        """
-        # Write your code here
-
-    def dequeue_character(self):
-        """
-        Do dequeue operation if the queue is not empty.
-        Returns:
-          The data that is dequeued if the queue is not empty.
-        """
-        # Write your code here
-
-
-# read the string text
-text = input()
-
-# find the length of text
-length_of_text = len(text)
-
-# Create the Solution class object
-solution = Solution(length_of_text)
-
-# push/enqueue all the characters of string text to stack
-for index in range(length_of_text):
-    # Write code here
-
-is_palindrome = True
-'''
-pop the top character from stack
-dequeue the first character from queue
-compare both characters
-If the comparison fails, set is_palindrome as False.
-'''
-# Write the necessary logic
-
-
-# finally print whether string text is palindrome or not.
-if is_palindrome:
-    print("The word, " + text + ", is a palindrome.")
-else:
-    print("The word, " + text + ", is not a palindrome.")
+for char in text:
+    if len(stack) == size:
+        print("The stack is full.")
+        else:
+            stack.append(char)
+            top += 1
+            if rear == (size - 1):
+                print("The queue is full.")
+                else:
+                    queue.append(char)
+                    if size % 2 == 0:
+                        c = len(text)
+                        else:
+                            c = (len(text) // 2) + 1
+                            is_palindrome = True
+                            for x in range(c):
+                                if top == -1:
+                                    print("stack is empty")
+                                    else:
+                                         data = stack.pop()
+                                            top -= 1
+                                            if rear == top == -1:
+                                                print("The queue is empty.")
+                                                else:
+                                                    data_ = queue.pop(0)
+                                                    if data == data_:
+                                                        is_palindrome = True
+                                                        else:
+                                                            is_palindrome = False
+                                                            break
+                                                            if is_palindrome:
+                                                                print("The word, " + text + ", is a palindrome.")  
+                                                                else:
+                                                                    print("The word, " + text + ", is not a palindrome.")
